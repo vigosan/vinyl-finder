38339e552b3c84cd1fd2e6a4cbe8aad234e7a5db
Vicent Gozalbes <vigosan@gmail.com>
2023-09-13 08:12:43 +0200

    Update discogs.spec.js

--- tests/discogs.spec.js
+++ tests/discogs.spec.js
@@ -3,7 +3,6 @@ const { test, expect } = require("@playwright/test");
 const NO_ITEMS = "No items for sale for this Release.";
 const RELEASE_URI = "http://www.discogs.com/release";
 const VINYL_IDS = [
-  "1046451-Ultrabeat-V-Darren-Styles-Sure-Feels-Good",
   "1903354-Paradygma-Stay-With-Me",
   "1264330-Digital-Dunk-Kye-Shand-Wayne-Ewing-Fuckin-Jew-Suck-My-Balls",
 ];
