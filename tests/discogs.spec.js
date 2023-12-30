1b641081058f15094562d9697316ae234474b818
Vicent Gozalbes <vigosan@gmail.com>
2023-12-30 13:55:43 +0100

    Update discogs.spec.js

--- tests/discogs.spec.js
+++ tests/discogs.spec.js
@@ -3,7 +3,6 @@ const { test, expect } = require("@playwright/test");
 const NO_ITEMS = "No items for sale for this Release.";
 const RELEASE_URI = "http://www.discogs.com/release";
 const VINYL_IDS = [
-  "1903354-Paradygma-Stay-With-Me",
   "9725232-DJ-Made-Goodbye-Cuchara-Tech",
   "658976-Dani-EP-vs-DJ-Frank-Pure-Milk",
 ];
